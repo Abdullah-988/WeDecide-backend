ffc3844de9e585e3e010d8ac8f10033576ea6cc2
Abdullah Abdulkareem <channel.redrex@gmail.com>
2024-05-11 02:29:55 +0300

    Removed pusher TLS

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -1,9 +1,8 @@
 import PusherServer from "pusher";
 
 export const pusherServer = new PusherServer({
-  appId: process.env.PUSHER_APP_ID!,
+  appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  secret: process.env.PUSHER_APP_SECRET!,
+  secret: process.env.NEXT_PUBLIC_PUSHER_APP_SECRET!,
   cluster: "eu",
-  useTLS: true,
 });
